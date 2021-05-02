ef30fb5d3de5d44b14a32259b831eef5c2280f3d
dkstjdwns114 <sungjoon256@gmail.com>
2021-05-02 22:35:49 +0900

    customer create controller

--- app/Controllers/CustomerController.php
+++ app/Controllers/CustomerController.php
@@ -6,10 +6,12 @@ use App\Models\CustomerModel;
 class CustomerController extends Controller
 {
   protected $customer;
+  protected $request;
 
   public function __construct()
   {
     $this->customer = new CustomerModel();
+    $this->request = \Config\Services::request();
   }
 
   public function index()
@@ -21,5 +23,24 @@ class CustomerController extends Controller
   {
     $data = $this->customer->findAll();
     return json_encode($data);
+  }
+
+  public function create()
+  {
+    try {
+      $json = $this->request->getJSON();
+      $insert['name'] = $json->name;
+      $insert['email'] = $json->email;
+      $insert['phone'] = $json->phone;
+      $insert['address'] = $json->address;
+      $res = $this->customer->insert($insert);
+      $response['success'] = true;
+      $response['message'] = "Successful save";
+      return json_encode($response);
+    }catch (\Exception $e){
+      $responsep['success'] = false;
+      $response['message'] = $e->getMessage();
+      return json_encode($response);
+    }
   }
 }
